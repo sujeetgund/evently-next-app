3c1adcabd45f1686fdf4565433e4c55245a93478
Sujeet Gund <sujeetgund@gmail.com>
2024-06-11 17:20:22 +0530

    implement clerk auth and create header and footer

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen w-full flex-center bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+      <SignIn />
+    </main>
+  );
+}
